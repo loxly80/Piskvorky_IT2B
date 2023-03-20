8b1fe8cad78b180512ef885c002a87b5c1dd5818
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-03-20 14:01:14 +0100

    před hodinou

---
+++ Piskvorky_IT2B/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piskvorky_IT2B
+{
+  public class Game
+  {
+    public List<GameObject> GameObjects { get; } = new List<GameObject>();
+    public int Top
+    {
+      get
+      {
+        if(GameObjects.Count == 0)
+        {
+          return 0;
+        } 
+        else if(GameObjects.Count == 1)
+        {
+          return 5;
+        }
+        else
+        {
+          int top = 0;
+          foreach(GameObject obj in GameObjects)
+          {
+            if(obj.Y > top)
+            {
+              top = obj.Y;
+            }
+          }
+          return top + 5;
+        }
+      }
+    }
+    public int Bottom
+    {
+      get
+      {
+        if (GameObjects.Count == 0)
+        {
+          return 0;
+        }
+        else if (GameObjects.Count == 1)
+        {
+          return 5;
+        }
+        else
+        {
+          int bottom = 0;
+          foreach (GameObject obj in GameObjects)
+          {
+            if (obj.Y < bottom)
+            {
+              bottom = obj.Y;
+            }
+          }
+          return Math.Abs(bottom) + 5;
+        }
+      }
+    }
+  }
+}
---
+++ Piskvorky_IT2B/GameObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piskvorky_IT2B
+{
+  public class GameObject
+  {
+    public int X { get; }
+    public int Y { get; }
+    public ValueType Value { get; }
+
+    public GameObject(int x, int y, ValueType value)
+    {
+      X = x;
+      Y = y;
+      Value = value;
+    }
+
+    public enum ValueType
+    {
+      Circle,
+      Cross
+    }
+  }
+}
